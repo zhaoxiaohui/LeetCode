660dd10b904572a68bc8c3a5ea832dbdce2890c1
zhaohui11 <fire.guy@163.com>
2015-11-15 12:49:10 +0800

    Update code

--- Add and Search Word.cpp
+++ Add and Search Word.cpp
@@ -5,37 +5,37 @@
  *          Date: 2015-11-15
  */
 
-struct TireNode {
-    TireNode *children[26];
+struct TrieNode {
+    TrieNode *children[26];
     string word;
-    TireNode() {
-        memset(children, 0, sizeof(TireNode *) * 26);
+    TrieNode() {
+        memset(children, 0, sizeof(TrieNode *) * 26);
         word = "";
     }
 };
 
 class WordDictionary {
 public:
-    TireNode *root;
+    TrieNode *root;
     WordDictionary() {
-        root = new TireNode();
+        root = new TrieNode();
     }
     // Adds a word into the data structure.
     void addWord(string word) {
         const char *str = word.c_str();
-        TireNode *cur_node = root;
+        TrieNode *cur_node = root;
         while(*str) {
             if (cur_node->children[*str - 'a'] == NULL) {
-                cur_node->children[*str - 'a'] = new TireNode();
+                cur_node->children[*str - 'a'] = new TrieNode();
             }
             cur_node = cur_node->children[*str - 'a'];
             str++;
         }
         cur_node->word = word;
     }
-    bool match(const char *str, int deep, int len, TireNode *cur_node) {
+    bool match(const char *str, int deep, int len, TrieNode *cur_node) {
         if (deep == len) {
-            return !cur_node->word.empty();;
+            return !cur_node->word.empty();
         }
         if (*str != '.') {
             return cur_node->children[*str - 'a'] && match(str + 1, deep + 1, len, cur_node->children[*str - 'a']);
@@ -54,8 +54,3 @@ public:
         return match(word.c_str(), 0, word.length(), root);
     }
 };
-
-// Your WordDictionary object will be instantiated and called as such:
-// WordDictionary wordDictionary;
-// wordDictionary.addWord("word");
-// wordDictionary.search("pattern");
